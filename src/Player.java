8af88bada56a0c3d87123f853ad0cbadd44a734d
Asxchksel <axel.mannheimer@elev.ga.ntig.se>
2024-03-20 11:03:52 +0100

    made classes

---
+++ src/Player.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Player {
+    private String name;
+    private final ArrayList<String> ownedPokemon = new ArrayList<String>();
+
+    public void usePokemon(){
+
+    }
+}
---
+++ src/Pokemon.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class Pokemon {
+    private int health;
+    private String name;
+    protected String type;
+    private Random dodge;
+
+    public void Attack() {
+
+    }
+
+    public double dodgeAttack(Random dodge){
+        double dodgePercent = 0;
+        return dodgePercent;
+    }
+}
